4fae6889d60d65ae2649155a57a184ae7ac8ac1a
aitor <aitor.cavia@opendeusto.es>
2022-12-11 20:41:39 +0100

    solution

--- src/main/java/com/tasks/config/WebSecurityConfig.java
+++ src/main/java/com/tasks/config/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
 		web
 		.ignoring()
-		.antMatchers("/tasks-service/**")
+		.antMatchers("/application/**")
 		.antMatchers("/css/**")
 		.antMatchers("/javascript-libs/noty/**")
 		.antMatchers("/react-libs/**")
